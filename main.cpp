d5e1de5e8c75aa023ab969d921721bb18a6c38c9
Piotr Wachowicz <pwachowicz98@gmail.com>
2020-05-12 18:13:10 +0200

    Konstruktor tworzy liste od konca. Zjadanie

--- main.cpp
+++ main.cpp
@@ -3,43 +3,44 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <QDebug>
 
 class Stworzenie
 {
 public:
     std::string nazwa;
     int wielkosc;
+    static Stworzenie* head;
     Stworzenie* next;
+    static int iloscStworzen;
 
-    static void nowy(Stworzenie*& head, Stworzenie* nowe);
     virtual void rosnij() = 0;
     virtual void rozmnazajSie() = 0;
     virtual void przezyjDzien() = 0;
-};
 
-//powiniennem stworzyc interfejs class list po ktorej będzie dziedziczolo Stworzenie. Lista bedzie zawierala metody do obslugi listy
-void Stworzenie::nowy(Stworzenie*& head, Stworzenie* nowe)
-{
-    if (head == nullptr)
-        head = nowe;
-    else {
-        Stworzenie* looking = head;
-        while(looking->next != nullptr)
-            looking = looking->next;
-        looking->next = nowe;
+    virtual ~Stworzenie()
+    {
+        std::cout << "Wywolano destruktor Stowrzenia" << std::endl;
     }
-}
+};
 
 class Roslina : public Stworzenie
 {
 public:
     static int iloscRoslin;
+
     Roslina()
     {
         nazwa = "roslina";
         wielkosc = 1;
         iloscRoslin++;
-        next = nullptr;
+        iloscStworzen++;
+        next = head;
+        head = this;
+    }
+    ~Roslina()
+    {
+        std::cout << "Wywolano destruktor rosliny" << std::endl;
     }
     void rosnij()
     {
@@ -47,12 +48,7 @@ public:
     }
     void rozmnazajSie()
     {
-//        Stworzenie* nowe = new Roslina;
-//        Stworzenie* temp = head;
-//        while(temp != nullptr)
-//            temp = temp->next;
-//        temp->next = nowe;
-//        nowe->next = nullptr;
+        Stworzenie* nowe = new Roslina();
     }
     void przezyjDzien()
     {
@@ -65,6 +61,11 @@ class Slimak : public Stworzenie
 {
 public:
     int wiek;
+
+    virtual ~Slimak()
+    {
+        std::cout << "Wywolano destruktor slimaka" << std::endl;
+    }
     void rosnij()
     {
         wielkosc = wielkosc * 1.1;
@@ -88,32 +89,48 @@ class RoslinozernySlimak : public Slimak
 {
 public:
     static int iloscRoslinozernychSlimakow;
+
     RoslinozernySlimak()
     {
         nazwa = "roslinozernySlimak";
         wielkosc = 10;
         iloscRoslinozernychSlimakow++;
-        next = nullptr;
+        iloscStworzen++;
+        next = head;
+        head = this;
+    }
+    ~RoslinozernySlimak()
+    {
+        std::cout << "Wywolano destruktor roslinozernego slimaka" << std::endl;
     }
     void rozmnazajSie()
     {
-//        Stworzenie* nowe = new RoslinozernySlimak;
-//        Stworzenie* temp = head;
-//        while(temp != nullptr)
-//            temp = temp->next;
-//        temp->next = nowe;
-//        nowe->next = nullptr;
+        Stworzenie* nowe = new RoslinozernySlimak();
     }
     void zjedz()
     {
-//        if (Roslina::iloscRoslin == 0)
-//            std::cout << "Wszystkie rosliny zostaly zniszczone! " << std::endl;
-//        else {
-//            int zjadany;
-//            do{
-//                zjadany = rand() % stworzenia.size();
-//            }while(stworzenia[zjadany]->nazwa != "roslina");
-//        }
+        if (Roslina::iloscRoslin == 0)
+            std::cout << "Wszystkie rosliny zostaly zniszczone! " << std::endl;
+        else {
+            int zjadany;
+            Stworzenie* zjadane;
+            do{
+                zjadany = rand() % iloscStworzen;
+                zjadane = head;
+                for (int i = 0; i < zjadany; i++)
+                    zjadane = zjadane->next;
+                std::cout << "zjadane: " << zjadane->nazwa <<", " << zjadane->wielkosc << std::endl;
+            }while(zjadane->nazwa != "roslina");
+
+            //zamiast tego wprowadze Stworzenie* previous i usuwanie bedzie odbywalo sie w destruktorach
+            zjadane = head;
+            for (int i = 0; i < zjadany - 1; i++)
+                zjadane = zjadane->next;
+            zjadane->next = zjadane->next->next;
+            Roslina::iloscRoslin--;
+            Stworzenie::iloscStworzen--;
+            delete zjadane;
+        }
     }
 };
 
@@ -121,38 +138,46 @@ class DrapieznySlimak : public Slimak
 {
 public:
     static int iloscDrapieznychSlimakow;
+
     DrapieznySlimak()
     {
         nazwa = "drapieznySlimak";
         wielkosc = 10;
         iloscDrapieznychSlimakow++;
-        next = nullptr;
+        iloscStworzen++;
+        next = head;
+        head = this;
+    }
+    ~DrapieznySlimak()
+    {
+        std::cout << "Wywolano destruktor drapieznego slimaka" << std::endl;
     }
     void rozmnazajSie()
     {
-//        Stworzenie* nowe = new DrapieznySlimak;
-//        Stworzenie* temp = head;
-//        while(temp != nullptr)
-//            temp = temp->next;
-//        temp->next = nowe;
-//        nowe->next = nullptr;
+        Stworzenie* nowe = new DrapieznySlimak();
     }
     void zjedz()
     {
-//        if (RoslinozernySlimak::iloscRoslinozernychSlimakow == 0)
-//            std::cout << "Wszystkie roslinozerne slimaki nie zyja!" << std::endl;
-//        else {
-//            int zjadany;
-//            do{
-//                zjadany = rand() % stworzenia.size();
-//            }while(stworzenia[zjadany]->nazwa != "roslinozernySlimak");
-//        }
+        if (RoslinozernySlimak::iloscRoslinozernychSlimakow == 0)
+            std::cout << "Wszystkie roslinozerne slimaki nie zyja!" << std::endl;
+        else {
+            Stworzenie* zjadane;
+            do{
+                int zjadany = rand() % iloscStworzen;
+                zjadane = head;
+                for (int i = 0; i < zjadany; i++)
+                    zjadane = zjadane->next;
+                std::cout << "zjadane: " << zjadane->nazwa <<", " << zjadane->wielkosc << std::endl;
+            }while(zjadane->nazwa != "roslinozernySlimak");
+        }
     }
 };
 
 int Roslina::iloscRoslin;
 int RoslinozernySlimak::iloscRoslinozernychSlimakow;
 int DrapieznySlimak::iloscDrapieznychSlimakow;
+int Stworzenie::iloscStworzen;
+Stworzenie* Stworzenie::head;
 
 int main(int argc, char *argv[])
 {
@@ -165,24 +190,24 @@ int main(int argc, char *argv[])
     Roslina::iloscRoslin = 0;
     RoslinozernySlimak::iloscRoslinozernychSlimakow = 0;
     DrapieznySlimak::iloscDrapieznychSlimakow = 0;
+    Stworzenie::iloscStworzen = 0;
 
-    Stworzenie* head = nullptr;
+    //Stworzenie* head = nullptr;
+    Stworzenie::head = nullptr;
 
-    {Stworzenie* nowe = new RoslinozernySlimak;
-    Stworzenie::nowy(head, nowe);}
+    {Stworzenie* nowe = new RoslinozernySlimak();}
+    {Stworzenie* nowe = new Roslina();}
+    {Stworzenie* nowe = new DrapieznySlimak();}
 
-    {Stworzenie* nowe = new Roslina;
-    Stworzenie::nowy(head, nowe);}
-
-    {Stworzenie* nowe = new DrapieznySlimak;
-    Stworzenie::nowy(head, nowe);}
-
-    Stworzenie* temp = head;
-    while (temp != nullptr) {
-        std::cout << temp->nazwa <<", " << temp->wielkosc << std::endl;
-        temp->przezyjDzien();
-//        std::cout << temp->nazwa <<", " << temp->wielkosc << std::endl;
-        temp = temp->next;
+    for (int i=0; i<2; i++) {
+        Stworzenie* temp = Stworzenie::head;
+        std::cout << "Dzien " << i << std::endl;
+        while (temp != nullptr) {
+            std::cout << temp->nazwa <<", " << temp->wielkosc << std::endl;
+            temp->przezyjDzien();
+            std::cout << temp->nazwa <<", " << temp->wielkosc << std::endl;
+            temp = temp->next;
+        }
     }
     return 0;
     return a.exec();
