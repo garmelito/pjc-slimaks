7eb466d9cd3208e0109ef1447f0f40a6c02714c4
Piotr Wachowicz <pwachowicz98@gmail.com>
2020-05-26 18:46:24 +0200

    fix

--- symulacja.cpp
+++ symulacja.cpp
@@ -1,9 +1,8 @@
 #include "symulacja.h"
 
 Symulacja::Symulacja() {
-    std::shared_ptr<std::deque<Stworzenie*>> stworzenia = std::make_shared<std::deque<Stworzenie*>>();
-    std::shared_ptr<std::deque<Stworzenie*>> rosliny = std::make_shared<std::deque<Stworzenie*>>();
-    std::shared_ptr<std::deque<Stworzenie*>> roslinozerneSlimaki = std::make_shared<std::deque<Stworzenie*>>();
-    std::shared_ptr<std::deque<Stworzenie*>> drapiezneSlimaki = std::make_shared<std::deque<Stworzenie*>>();
-    std::cout << stworzenia->empty() << std::endl;
+    stworzenia = std::make_shared<std::deque<Stworzenie*>>();
+    rosliny = std::make_shared<std::deque<Stworzenie*>>();
+    roslinozerneSlimaki = std::make_shared<std::deque<Stworzenie*>>();
+    drapiezneSlimaki = std::make_shared<std::deque<Stworzenie*>>();
 }
